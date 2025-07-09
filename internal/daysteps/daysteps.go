da816a259f667af6680791e934d1fe026ce22b8e
Slayder12 <gnutenko.s.r@gmail.com>
2025-07-09 18:10:35 +1000

    update

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,14 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +19,59 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+
+	splitData := strings.Split(data, ",")
+	if len(splitData) != 2 {
+		return 0, 0, errors.New("неправильное количество параметров")
+	}
+
+	steps, err := strconv.Atoi(splitData[0])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if steps <= 0 {
+		return 0, 0, errors.New("неверное значение шагов")
+	}
+
+	duration, err := time.ParseDuration(splitData[1])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+
+	if !spentcalories.ValidateWeight(weight) {
+		fmt.Println("неверное значение веса", weight)
+		return ""
+	}
+	if !spentcalories.ValidateHeight(height) {
+		fmt.Println("неверное значение роста", height)
+		return ""
+	}
+
+	steps, duration, err := parsePackage(data)
+
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	distance := (float64(steps) * stepLength) / mInKm
+	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	dayActionInfo := fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+		steps, distance, calories,
+	)
+
+	return dayActionInfo
 }
--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,6 +1,11 @@
 package spentcalories
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,28 +16,180 @@ const (
 	minInH                     = 60   // количество минут в часе.
 	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе
+
+	minWeight = 2.0   // Минимальный допустимый вес (кг)
+	maxWeight = 635.0 // Максимальный допустимый вес (кг)
+	minHeight = 0.50  // Минимальный допустимый рост (м)
+	maxHeight = 2.75  // Максимальный допустимый рост (м)
 )
 
 func parseTraining(data string) (int, string, time.Duration, error) {
-	// TODO: реализовать функцию
+	splitData := strings.Split(data, ",")
+	if len(splitData) != 3 {
+		return 0, "", 0, errors.New("неправильное количество параметров")
+	}
+
+	steps, err := strconv.Atoi(splitData[0])
+	if err != nil {
+		return 0, "", 0, err
+	}
+
+	if steps <= 0 {
+		return 0, "", 0, errors.New("неверное значение шагов")
+	}
+
+	duration, err := time.ParseDuration(splitData[2])
+	if err != nil {
+		return 0, "", 0, err
+	}
+
+	if duration == 0 {
+		fmt.Println("неверная продолжительность - ноль")
+		return 0, "", 0, err
+	}
+
+	return steps, splitData[1], duration, nil
 }
 
 func distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+
+	if !ValidateHeight(height) {
+		fmt.Println("неверное значение роста:", height)
+		return 0
+	}
+
+	if steps <= 0 {
+		fmt.Println("неверное значение шагов:", steps)
+		return 0
+	}
+
+	stepLength := height * stepLengthCoefficient
+	distance := (float64(steps) * stepLength) / mInKm
+	return distance
 }
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+
+	if duration <= 0 {
+		fmt.Println("неверное значение продолжительности:", duration)
+		return 0
+	}
+
+	distance := distance(steps, height)
+	speed := distance / duration.Hours()
+
+	return speed
 }
 
 func TrainingInfo(data string, weight, height float64) (string, error) {
-	// TODO: реализовать функцию
+
+	if !ValidateWeight(weight) {
+		return "", errors.New("неверное значение веса")
+	}
+
+	if !ValidateHeight(height) {
+		return "", errors.New("неверное значение роста")
+	}
+
+	steps, typeActivity, duration, err := parseTraining(data)
+
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	distance := distance(steps, height)
+	meanSpeed := meanSpeed(steps, height, duration)
+
+	var caloriesBurned float64
+
+	switch typeActivity {
+	case "Бег":
+		caloriesBurned, err = RunningSpentCalories(steps, weight, height, duration)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+	case "Ходьба":
+		caloriesBurned, err = WalkingSpentCalories(steps, weight, height, duration)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+	default:
+		return "", errors.New("неизвестный тип тренировки")
+	}
+
+	trainingInfo := fmt.Sprintf(
+		"Тип тренировки: %s\n"+
+		"Длительность: %.2f ч.\n"+
+		"Дистанция: %.2f км.\n"+
+		"Скорость: %.2f км/ч\n"+
+		"Сожгли калорий: %.2f",
+		typeActivity, duration.Hours(), distance, meanSpeed, caloriesBurned,
+	)
+
+	return trainingInfo, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+
+	if steps <= 0 {
+		return 0, errors.New("неверное значение шагов")
+	}
+
+	if !ValidateWeight(weight) {
+		return 0, errors.New("неверное значение веса")
+	}
+
+	if !ValidateHeight(height) {
+		return 0, errors.New("неверное значение роста")
+	}
+
+	if duration <= 0 {
+		return 0, errors.New("неверное значение продолжительности:")
+	}
+
+	speed := meanSpeed(steps, height, duration)
+	caloriesBurned := (weight * speed * duration.Minutes()) / minInH
+
+	return caloriesBurned, nil
 }
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+
+	caloriesBurned, err := RunningSpentCalories(steps, weight, height, duration)
+
+	if err != nil {
+		return 0, err
+	}
+
+	walkingCalories := caloriesBurned * walkingCaloriesCoefficient
+
+	return walkingCalories, nil
+}
+
+// Функция проверки веса (в килограммах)
+func ValidateWeight(weight float64) bool {
+
+	if weight < minWeight {
+		return false
+	}
+	if weight > maxWeight {
+		return false
+	}
+	return true
+}
+
+// Функция проверки роста (в метрах)
+func ValidateHeight(height float64) bool {
+
+	if height < minHeight {
+		return false
+	}
+	if height > maxHeight {
+		return false
+	}
+
+	return true
 }
